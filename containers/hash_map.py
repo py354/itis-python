5025224bf381e5cc70170452d4108fa9430f5c9a
Danis Kirasirov <dbgbbu@gmail.com>
2022-03-20 17:34:36 +0300

    Документация HashMap

--- containers/hash_map.py
+++ containers/hash_map.py
@@ -4,6 +4,8 @@ from containers.linked_list import LinkedList
 
 
 class HashMap(Map):
+    """ Реализация Map на основе хеш-таблицы из ячеек LinkedList """
+
     LOAD_FACTOR_TO_INCREASE = 0.75
     LOAD_FACTOR_TO_DECREASE = 0.1
 
@@ -13,17 +15,20 @@ class HashMap(Map):
         self.buckets: List[LinkedList] = [LinkedList() for _ in range(capacity)]
 
     def __iter__(self):
+        """ Итерация по словарю (ключ, значение) """
         for bucket in self.buckets:
             for item in bucket:
                 yield item
 
     def __resize(self, capacity):
+        """ Пересоздание таблицы с новым размером """
         self.capacity = capacity
         new_buckets = [LinkedList() for _ in range(capacity)]
         for key, value in self:
             new_buckets[hash(key) % capacity].append(key, value)
 
     def __setitem__(self, key, value):
+        """ Добавление или изменение элемента по ключу """
         bucket = self.buckets[hash(key) % self.capacity]
         node = bucket.get(key)
         if node is None:
@@ -36,6 +41,7 @@ class HashMap(Map):
             node.value = value
 
     def __delitem__(self, key):
+        """ Удаление элемента по ключу """
         bucket = self.buckets[hash(key) % self.capacity]
         if bucket.delete(key):
             self.length -= 1
@@ -44,6 +50,7 @@ class HashMap(Map):
                 self.__resize(self.capacity // 2)
 
     def __getitem__(self, key):
+        """ Получение элемента по ключу """
         bucket = self.buckets[hash(key) % self.capacity]
         node = bucket.get(key)
         if node is None:
